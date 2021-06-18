40069948dbb3c9b4cbb04fafe30fe312bb210db5
jonte24680 <35293410+jonte24680@users.noreply.github.com>
2021-06-18 12:25:07 +0200

    update host side
    made host side more functinal

--- source/Client/js/host.ts
+++ source/Client/js/host.ts
@@ -1,15 +1,33 @@
-var socket:SocketIOClient.Socket = io();
-
-
+var roomidElement = document.getElementsByClassName("info-roomID")[0];
 var roomid = Number(prompt("Custume id", "10"))
+function ChangeRoomID(id: number){
+    roomid = id
+    roomidElement.textContent = `RoomId: ${roomid}`
+}
+
+var playersElement = document.getElementById("players");
+
+var socket:SocketIOClient.Socket = io();
 
 socket.emit("new-room", roomid);
 
 socket.on("players-update", (room: any) => {
     console.log(room);
+    ChangeRoomID(room.roomID)
+
+    playersElement.innerHTML = ""
+    room.allPlayers.forEach((player) => {
+        playersElement.innerHTML += `        <div class="player">
+        <p class="player-name">${player.username}</p>
+        <p class="player-money">Money: ${player.money} $</p>
+        <p class="player-bet">Bet: ${player.bet} $</p>
+        <div class="player-card-images">
+            <img src="assets/images/Cards/gray_back.png" class="player-cards"/><img src="assets/images/Cards/QC.png" class="player-cards"/>
+        </div>
+    </div>`
+    });
 });
 
-console.log("host")
 
 /*
 function KeyPress(e) {
