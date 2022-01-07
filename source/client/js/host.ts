de13cff6565d6d0f27ebab8f951781bea0eeed46
jonte24680 <35293410+jonte24680@users.noreply.github.com>
2022-01-07 23:52:01 +0100

    added tabel cards

--- source/client/js/host.ts
+++ source/client/js/host.ts
@@ -20,6 +20,9 @@ socket.on("players-update", (Room: any) => {
 function UpdateGUI(){
     roomidElement.textContent = `RoomId: ${room.roomID}`
 
+    UpdateGUIStat(room)
+
+    // may be wonrebel to cross site scripting
     playersElement.innerHTML = ""
     room.players.forEach((player) => {
         playersElement.innerHTML += `        <div class="player" style="${player.id == room.gameInfo.turnPlayerId ? "background-color:hsl(119, 100%, 80%);" : ""}">
@@ -27,23 +30,38 @@ function UpdateGUI(){
         <p class="player-money">Money: ${player.money} $</p>
         <p class="player-bet">Bet: ${player.gameStat.bet} $</p>
         <div class="player-card-images">
-            ${AddCardImages(player)}
+            ${AddCardImagesToPlayer(player)}
         </div>
     </div>`
     });
 }
 
-function AddCardImages(player): string{
+function UpdateGUIStat(room) {
+    var statHTML = document.getElementById("stat");
+
+    var cards = document.getElementById("stat-cards");
+    var stringCards = "";
+    if(room.gameInfo.cards.length > 0){
+        room.gameInfo.cards[0].cards.forEach(card => {stringCards += GetCardImagesString(card, "stat-card")});
+    }
+    cards.innerHTML = stringCards
+}
+
+function AddCardImagesToPlayer(player): string{
     // Base <img src=assets/images/Cards/green_back.png" class="player-cards"/>
     var output: string = "";
     player.gameStat.cards.forEach(cardInfo => {
         if(cardInfo.name == "Hand"){
             cardInfo.cards.forEach(card => {
-                output += `<img src="assets/images/Cards/${CardPath(card)}.png" class="player-cards"/>`
+                output += GetCardImagesString(card, "player-cards");
             });
         }
     });
     return output;
+}
+
+function GetCardImagesString(card: any, className: string) {
+    return `<img src="assets/images/Cards/${CardPath(card)}.png" class="${className}"/>`
 }
 
 function CardPath(card: string): string{
@@ -66,3 +84,4 @@ newGameButtonElement.addEventListener("click", () => {
     socket.emit("host-new-game", gameName);
 
 });
+
